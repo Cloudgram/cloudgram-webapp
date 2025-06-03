8892da2c7410ec9b92ac34d53c3812e96b7e2bd7
Vega7 <ankuverindustries@gmail.com>
2025-06-03 03:50:23 +0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -48,4 +48,9 @@ export default defineConfig({
             '@shared': path.resolve(__dirname, 'src/shared'),
         },
     },
+    css: {
+        modules: {
+            localsConvention: 'camelCase',
+        },
+    },
 });
