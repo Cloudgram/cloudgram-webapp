7ec4fb0e3cccc11c78a48fd3707413bea0ab1812
Vampika <vampika.vamp@gmail.com>
2025-09-13 14:18:12 +0300

    wrapper for menu

--- src/entities/folder/ui/FolderCard.tsx
+++ src/entities/folder/ui/FolderCard.tsx
@@ -1,7 +1,7 @@
 import { FolderIconSmall } from '@/shared/assets/icons/all/FolderIconSmall';
 import type { folderCardArgs } from '../types/folder.types';
 import styles from './FolderCard.module.scss';
-import { ContextMenu } from '@/shared/ui/PopoverMenu/ContextMenu';
+import { FolderCardMenu } from './FolderCardMenu';
 import { useState } from 'react';
 
 export const FolderCard = ({ folderData, onDoubleClick }: folderCardArgs) => {
@@ -18,17 +18,9 @@ export const FolderCard = ({ folderData, onDoubleClick }: folderCardArgs) => {
                 <span></span>
                 <span></span>
                 <span></span>
-                <ContextMenu
+                <FolderCardMenu
                     isOpen={contextMenuOpen}
                     onClose={() => setContextMenuOpen(false)}
-                    items={[
-                        {
-                            content: <p>Переименовать</p>,
-                            onClick: () => console.log('rename'),
-                        },
-                        { content: <p>Скачать</p>, onClick: () => console.log('download') },
-                        { content: <p>Удалить</p>, onClick: () => console.log('delete') },
-                    ]}
                 />
             </div>
         </div>
